8d76480838f6d829295015873ec28a04d35658a0
Marcelo <correodemarcelopalacios@gmail.com>
2020-06-30 13:29:15 -0300

    repositorio-nodejs-crud-mysql public

---
+++ routes/customer.js
@@ -0,0 +1,12 @@
+// requiero el modulo express
+const express = require('express');
+// ejecutaremos el metodo router
+// devuelve un objeto de js que 
+// puedo ir agregandole rutas.
+const router = express.Router();
+
+
+
+
+// al final lo exporto
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,17 +1,46 @@
-// vamos a requerir express
+// cargo el módulo
 const express = require('express');
-// una vez requerido lo inicializamnos con una constante llamada app.
+// el modulo path une carpetas
+const path = require('path');
+// el módulo morgan contiene varias funciones
+// que utilizaremos en los middlewares
+const morgan = require('morgan')
+    // el módulo mysql gestiona la base de datos
+const mysql = require('mysql');
+// el modulo express-myConnection la conexión
+// a la base de datos
+const myConnection = require('express-myconnection');
+// el módulo express gestiona una librería de funciones
 const app = express();
 
+// importing routes
+const customerRoutes = require('./routes/customer')
+
 // settings
-// establezco el puerto de mi servidor 
-// revise que hay un puerto en el sistema operativo
-// si no lo encuentra utilice el puerto 3000
+app.use('/', customerRoutes);
+
+// static files
+
+// gestiona el puerto del servidor
 app.set('port', process.env.PORT || 3000);
+// gestiono el motor de la vista
+app.set('view engine', 'ejs');
+// indico donde estan las carpetas de las vistas
+app.set('views', path.join(__dirname, 'views'));
 
+// middlewares (Todos los middlewares son funciones)
+app.use(morgan('dev'));
+app.use(myConnection(mysql, {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'crudnodejsmysql'
+}, 'single'));
 
-// ahora vamos a inicializar el servidor
-// el siguiente log
+// routes
+
+// starting the server
 app.listen(app.get('port'), () => {
     console.log("Server en el puerto 3000");
 })
